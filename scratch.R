3890e7dcdf054c44accd7c39555ecc99ed9e8fb1
Daniel <danieljfeller@gmail.com>
2019-09-04 22:15:26 -0400

    Added UI widgets and column formatting

--- scratch.R
+++ scratch.R
@@ -1,21 +1,50 @@
 
+df <- read.csv('data/synthetic_patients.csv') %>%
+  mutate(VLS = ifelse(as.logical(VLS) == TRUE, "Yes", "No"),
+         drugAbuse = ifelse(as.logical(drugAbuse) == TRUE, "Yes", "No"), 
+         etohAbuse = ifelse(as.logical(etohAbuse) == TRUE, "Yes", "No"),
+         LTFU = ifelse(as.logical(LTFU) == TRUE, "Yes", "No"),
+         UnstableHousing = ifelse(as.logical(UnstableHousing) == TRUE, "Yes", "No"),
+         MissedApt = ifelse(as.logical(MissedApt) == TRUE, "Yes", "No"),
+         NewDx = ifelse(as.logical(NewDx) == TRUE, "Yes", "No"),
+         HCV = ifelse(as.logical(HCV) == TRUE, "Yes", "No"),
+         HTN = ifelse(as.logical(HTN) == TRUE, "Yes", "No"),
+         behavioralDx = ifelse(as.logical(behavioralDx) == TRUE, "Yes", "No"),
+         hospitalizationRisk = round(hospitalizationRisk, digits = 2)
+  )
 
 
-getCD4column <- 'CD4' %in% c('VLS', 'CD4')
-c('CD4')[getCD4column]
+patientColors <- c('#fc9272', '#fb9372', '#fb9473', '#fa9573', '#f99574', '#f99674', '#f89774', 
+                   '#f79875', '#f79975', '#f69a76', '#f59b76', '#f49b76', '#f49c77', '#f39d77', 
+                   '#f29e78', '#f29f78', '#f19f78', '#f0a079', '#efa179', '#efa27a', '#eea37a', 
+                   '#eda37a', '#eca47b', '#eca57b', '#eba67c', '#eaa67c', '#e9a77c', '#e9a87d', 
+                   '#e8a97d', '#e7aa7e', '#e6aa7e', '#e6ab7e', '#e5ac7f', '#e4ad7f', '#e3ad80', 
+                   '#e2ae80', '#e2af80', '#e1b081', '#e0b081', '#dfb182', '#deb282', '#ddb283', 
+                   '#ddb383', '#dcb483', '#dbb584', '#dab584', '#d9b685', '#d8b785', '#d7b785', 
+                   '#d7b886', '#d6b986', '#d5ba87', '#d4ba87', '#d3bb87', '#d2bc88', '#d1bc88', 
+                   '#d0bd89', '#cfbe89', '#cebe8a', '#cebf8a', '#cdc08a', '#ccc08b', '#cbc18b', 
+                   '#cac28c', '#c9c28c', '#c8c38d', '#c7c48d', '#c6c48d', '#c5c58e', '#c4c68e', 
+                   '#c3c68f', '#c2c78f', '#c1c88f', '#c0c890', '#bfc990', '#beca91', '#bdca91', 
+                   '#bbcb92', '#bacc92', '#b9cc92', '#b8cd93', '#b7ce93', '#b6ce94', '#b5cf94', 
+                   '#b4d095', '#b3d095', '#b1d195', '#b0d196', '#afd296', '#aed397', '#add397', 
+                   '#abd498', '#aad598', '#a9d598', '#a8d699', '#a6d799', '#a5d79a', '#a4d89a', 
+                   '#a2d89b', '#a1d99b')
 
-getCOSTcolumn <- 'Cost' %in% c('VLS', 'CD4')
-c('Cost')[getCOSTcolumn]
+ID_brks <- quantile(as.numeric(rownames(df)), probs = seq(.01, .99, .01), na.rm = TRUE)
+df$ID <- as.numeric(rownames(df))
 
-
-%>%
-  # add colors to continuous variables
-  formatStyle('CD4', 
-              backgroundColor = styleInterval(cd4_brks, gradientColors)) %>%
-  formatStyle('hospitalizationRisk', 
-              backgroundColor = styleInterval(hosp_risk_brks, gradientColors)) %>%
-  formatStyle('Cost', 
-              backgroundColor = styleInterval(cost_brks, gradientColors)) %>%
-  formatStyle('HbA1c', 
-              backgroundColor = styleInterval(hba1c_brks, gradientColors)) 
-
+DT::datatable(
+  df,
+  rownames = FALSE, 
+  #colnames = c(ID = 1),  # add the name 
+  extensions = 'RowReorder',
+  selection = 'none',
+  autoHideNavigation = TRUE,
+  options = list(fixedHeader = TRUE,
+                 pageLength = 100,
+                 rowReorder = TRUE)) %>%
+  formatStyle(columns = 'Name', 
+              valueColumns = 'ID',
+              fontWeight = 'bold',
+              backgroundColor = styleInterval(ID_brks, patientColors))
+  
--- selectVariables.R
+++ selectVariables.R
@@ -1,22 +1,28 @@
 library(shinyjqui)
 library(DT)
+library(shinythemes)
 
 ######################
 # load & format data #
 ######################
 
+toFactor <- function(x){
+  return(as.factor(ifelse(as.logical(x) == TRUE, "Yes", "No")))
+}
+
 
 df <- read.csv('data/synthetic_patients.csv') %>%
-  mutate(VLS = ifelse(as.logical(VLS) == TRUE, "Yes", "No"),
-         drugAbuse = ifelse(as.logical(drugAbuse) == TRUE, "Yes", "No"), 
-         etohAbuse = ifelse(as.logical(etohAbuse) == TRUE, "Yes", "No"),
-         LTFU = ifelse(as.logical(LTFU) == TRUE, "Yes", "No"),
-         UnstableHousing = ifelse(as.logical(UnstableHousing) == TRUE, "Yes", "No"),
-         MissedApt = ifelse(as.logical(MissedApt) == TRUE, "Yes", "No"),
-         NewDx = ifelse(as.logical(NewDx) == TRUE, "Yes", "No"),
-         HCV = ifelse(as.logical(HCV) == TRUE, "Yes", "No"),
-         HTN = ifelse(as.logical(HTN) == TRUE, "Yes", "No"),
-         behavioralDx = ifelse(as.logical(behavioralDx) == TRUE, "Yes", "No")
+  mutate(VLS = toFactor(VLS),
+         drugAbuse =toFactor(drugAbuse), 
+         etohAbuse = toFactor(etohAbuse),
+         LTFU = toFactor(LTFU),
+         UnstableHousing = toFactor(UnstableHousing),
+         MissedApt = toFactor(MissedApt),
+         NewDx = toFactor(NewDx),
+         HCV = toFactor(HCV),
+         HTN = toFactor(HTN),
+         behavioralDx = toFactor(behavioralDx),
+         hospitalizationRisk = round(hospitalizationRisk, digits = 2)
   )
     
 
@@ -29,26 +35,61 @@ variablesNamed <- c('Risk of Hospitalization', 'Substance Abuse', 'Alcohol Abuse
 # prepare visualization #
 #########################
 
-gradientColors <- c('#E5F5E0','#E7F3DD','#EAF2DB','#ECF1D8','#EFEFD6','#F1EED4',
-                    '#F4EDD1','#F6EBCF','#F9EACC','#FBE9CA','#FEE8C8')
+patientColors <- c('#a1d99b', '#a2d89b', '#a4d89a', '#a5d79a', '#a6d799', '#a8d699', 
+                   '#a9d598', '#aad598', '#abd498', '#add397', '#aed397', '#afd296', '#b0d196', 
+                   '#b1d195', '#b3d095', '#b4d095', '#b5cf94', '#b6ce94', '#b7ce93', '#b8cd93', 
+                   '#b9cc92', '#bacc92', '#bbcb92', '#bdca91', '#beca91', '#bfc990', '#c0c890', 
+                   '#c1c88f', '#c2c78f', '#c3c68f', '#c4c68e', '#c5c58e', '#c6c48d', '#c7c48d', 
+                   '#c8c38d', '#c9c28c', '#cac28c', '#cbc18b', '#ccc08b', '#cdc08a', '#cebf8a', 
+                   '#cebe8a', '#cfbe89', '#d0bd89', '#d1bc88', '#d2bc88', '#d3bb87', '#d4ba87', 
+                   '#d5ba87', '#d6b986', '#d7b886', '#d7b785', '#d8b785', '#d9b685', '#dab584', 
+                   '#dbb584', '#dcb483', '#ddb383', '#ddb283', '#deb282', '#dfb182', '#e0b081', 
+                   '#e1b081', '#e2af80', '#e2ae80', '#e3ad80', '#e4ad7f', '#e5ac7f', '#e6ab7e', 
+                   '#e6aa7e', '#e7aa7e', '#e8a97d', '#e9a87d', '#e9a77c', '#eaa67c', '#eba67c', 
+                   '#eca57b', '#eca47b', '#eda37a', '#eea37a', '#efa27a', '#efa179', '#f0a079', 
+                   '#f19f78', '#f29f78', '#f29e78', '#f39d77', '#f49c77', '#f49b76', '#f59b76', 
+                   '#f69a76', '#f79975', '#f79875', '#f89774', '#f99674', '#f99574', '#fa9573', 
+                   '#fb9473', '#fb9372', '#fc9272')
+
+gradientColors <- c('#a1d99b', '#aed397', '#b9cc93', '#c4c68e', '#cdbf8a', 
+                    '#d6b986', '#dfb182', '#e7aa7e', '#eea27a', '#f59a76', '#fc9272')
 
 cd4_brks <- quantile(df$CD4, probs = seq(.05, .95, .10), na.rm = TRUE)
 hosp_risk_brks <- quantile(df$hospitalizationRisk, probs = seq(.05, .95, .10), na.rm = TRUE)
 cost_brks <- quantile(df$Cost, probs = seq(.05, .95, .10), na.rm = TRUE)
 hba1c_brks <- quantile(df$HbA1c, probs = seq(.05, .95, .10), na.rm = TRUE)
+
+# generates list for dplyr::arrange
+formatSort <- function(selectedOrder){
+  myList <- c()
+  # loop over each column in selectedOrder
+  for(i in 1:length(selectedOrder)){
+    myList <-c(myList, paste('desc(',selectedOrder[i],')', sep = ''))
+  }
+  # return unquoted objects
+  return(
+    print(as.name(paste(myList, collapse=', ')))
+  )
+}
     
 ##############
 # create UI #
 #############
     
-ui <- basicPage(
+ui <- fluidPage(theme = shinytheme("lumen"),
   h2("Who are my high-risk patients?"),
   
   sidebarLayout(
     sidebarPanel(width = 2,
-  # add sidebarPanel
+  # add sidebarPanel,
       sortableCheckboxGroupInput("selectedVariables", "Indicators",
-                                 choices = variables)
+                                 choices = variables,
+                                 selected = 'VLS'),
+  selectInput("sortMethod", "Sorting Method:",
+              c("No Sorting" = 'noSort',
+                "Hard Sorting" = "hardSort",
+                "Weighted Sorting" = "weightedSort")),
+  checkboxInput("rankRows", "Rank Sort Patients", value = FALSE)
     ),
     mainPanel(
       DT::dataTableOutput("patientDF")
@@ -62,6 +103,13 @@ ui <- basicPage(
 
 server <- function(input, output) {
   
+  output$order <- renderPrint({
+    formatSort(
+      input$selectedVariables_order[input$selectedVariables_order %in% input$selectedVariables]
+    )
+  })
+  
+
   # data table
   output$patientDF = DT::renderDT(server=FALSE,{
     
@@ -76,30 +124,38 @@ server <- function(input, output) {
     
     # plot data
       DT::datatable(
-        df[-1,] %>% select(c('Name', input$selectedVariables)),
+        df[-1,] 
+          %>%  select(c('Name', input$selectedVariables_order[input$selectedVariables_order %in% input$selectedVariables])) 
+         %>% arrange_at(
+           input$selectedVariables_order[input$selectedVariables_order %in% input$selectedVariables],
+           desc
+         )
+        ,
+        filter = 'top',
         rownames = FALSE, 
         colnames = c(ID = 1),  # add the name 
         extensions = 'RowReorder',
         selection = 'none',
         autoHideNavigation = TRUE,
-        options = list(pageLength = 100,
-                       order = list(list(0, 'asc')), 
+        options = list(fixedHeader = TRUE,
+                       pageLength = 100,
                        rowReorder = TRUE)) %>%
-        # add color to binary variables
+        # add color to binary variables 
+        formatStyle('ID', fontWeight = 'bold') %>%
         formatStyle(c('VLS', 'drugAbuse', 'etohAbuse', 'LTFU',
                       'UnstableHousing', 'MissedApt', 'NewDx', 'HCV',
                       'HTN', 'behavioralDx')[BinarySelection], 
-                    backgroundColor = styleEqual(c('No', 'Yes'), c('#e5f5e0', '#fee8c8')), 
+                    backgroundColor = styleEqual(c('No', 'Yes'), c('#a1d99b', '#fc9272')), 
                     fontWeight = 'bold') %>%
         # add colors to continuous variables
         formatStyle(c('CD4')[cd4Selection], 
-                    backgroundColor = styleInterval(cd4_brks, gradientColors)) %>%
+                    backgroundColor = styleInterval(cd4_brks, gradientColors), fontWeight = 'bold') %>%
         formatStyle(c('hospitalizationRisk')[riskSelection], 
-                    backgroundColor = styleInterval(hosp_risk_brks, gradientColors)) %>%
+                    backgroundColor = styleInterval(hosp_risk_brks, gradientColors), fontWeight = 'bold') %>%
         formatStyle(c('Cost')[costSelection], 
-                    backgroundColor = styleInterval(cost_brks, gradientColors)) %>%
+                    backgroundColor = styleInterval(cost_brks, gradientColors), fontWeight = 'bold') %>%
         formatStyle(c('HbA1c')[hbac1Selection], 
-                    backgroundColor = styleInterval(hba1c_brks, gradientColors)) 
+                    backgroundColor = styleInterval(hba1c_brks, gradientColors), fontWeight = 'bold') 
   })
 }
 
